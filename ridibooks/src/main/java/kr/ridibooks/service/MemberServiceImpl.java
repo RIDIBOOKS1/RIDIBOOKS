81cb3983ad8182db56790f910b679470a601201d
estelle-coding <estelle.coding@gmail.com>
2022-01-10 21:16:32 +0900

    sql

--- ridibooks/src/main/java/kr/ridibooks/service/MemberServiceImpl.java
+++ ridibooks/src/main/java/kr/ridibooks/service/MemberServiceImpl.java
@@ -6,15 +6,6 @@ import kr.ridibooks.model.MemberVO;
 public class MemberServiceImpl implements MemberService {
 
 	MemberDAO dao = new MemberDAO();
-//	public String register(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		MemberInsertController memberInsertController = new MemberInsertController();
-//		return memberInsertController.requestHandler(request, response);
-//	}
-//	
-//	public String Login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		MemberLoginController memberLoginController = new MemberLoginController();
-//		return memberLoginController.requestHandler(request, response);
-//	}
 	
 	@Override
 	public int register(MemberVO vo) {
