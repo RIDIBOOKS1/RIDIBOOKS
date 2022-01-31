351e152dccb870d44bc7fc6f03f41bc069196106
estelle-coding <estelle.coding@gmail.com>
2022-02-01 01:31:50 +0900

    회원 로그인된 상태에서 정보 변경(이메일, 이메일, 정보수신)시 전처리로써 비밀번호 확인

--- ridibooks/src/kr/ridibooks/controller/MemberModifyController.java
+++ ridibooks/src/kr/ridibooks/controller/MemberModifyController.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.reflection.SystemMetaObject;
+
 import kr.ridibooks.model.MemberVO;
 import kr.ridibooks.service.MemberServiceImpl;
 
@@ -31,14 +33,32 @@ public class MemberModifyController implements Controller {
 		idEmailVO.setEmail(email);
 		MemberVO foundVO = service.idEmailReturnVO(idEmailVO);
 		
+		// 로그인 안했은데 접근하여 응답코드 404
+		if(foundVO == null) {
+			response.setStatus(404);
+			System.out.println("로그인 하지 않음");
+			return null;
+		}
+		
+		// 찾아진 VO 테스트로 찍어보기
 		System.out.println("foundVO = " + foundVO);
 		
+		// 입력된 pw 값
 		String pw = request.getParameter("pw");
+		
+		if(pw == null || pw.isEmpty()) {
+			response.setStatus(400);
+			System.out.println("비밀번호 입력 안함");
+			return null;
+		}
 		
 		if(foundVO.getPw().equals(pw)) {
 			response.setStatus(200);
+			System.out.println("회원 로그인한 상태에서 회원 정보와 일치하는 비밀번호 입력");
 		} else {
 			response.setStatus(404);
+			System.out.println("회원정보의 비밀번호와 입력한 비밀번호의 불일치");
+			return null;
 		}
 		
 		return null;
